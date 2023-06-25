f623d72dc9446c15767e8a36a3f6eee45c8f4125
makenchi <lucas.santos9322@gmail.com>
2023-06-25 19:31:29 -0300

    feat: criando controller do webcrawler

---
+++ DotnetCrawlerAPI/DotnetCrawlerAPI/Controllers/WebCrawlerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetCrawlerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/webcrawl")]
+    public class WebCrawlerController : ControllerBase
+    {
+        [HttpGet("{searchOrigin}")]
+        public IEnumerable<string> SearchItens(string searchOrigin)
+        {
+            return Enumerable.Empty<string>();
+        }
+    }
+}
