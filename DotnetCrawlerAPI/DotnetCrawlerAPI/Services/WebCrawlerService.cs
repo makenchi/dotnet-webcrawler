f9967d042864f2d2a9dae71a14ee46d33448d4d7
makenchi <lucas.santos9322@gmail.com>
2023-06-25 21:11:26 -0300

    feat: instalando nuget do htmlagility e criando funções do service

--- DotnetCrawlerAPI/DotnetCrawlerAPI/Services/WebCrawlerService.cs
+++ DotnetCrawlerAPI/DotnetCrawlerAPI/Services/WebCrawlerService.cs
@@ -1,13 +1,30 @@
 ﻿using DotnetCrawlerAPI.DTO;
+using DotnetCrawlerAPI.Entities;
 using DotnetCrawlerAPI.Services.Interfaces;
+using HtmlAgilityPack;
 
 namespace DotnetCrawlerAPI.Services
 {
     public class WebCrawlerService : IWebCrawlerService
     {
-        public Task<List<ProductDTO>> SearchProducts(string searchOrigin, string query)
+        public async Task<List<ProductDTO>> SearchProducts(string searchOrigin, string query)
         {
             throw new NotImplementedException();
+        }
+
+        private List<Comment> getComments(string productUrl)
+        {
+            return new List<Comment>();
+        }
+
+        private List<Product> getProducts(string searchOrigin, string query)
+        {
+            return new List<Product>();
+        }
+
+        private HtmlDocument crawle(string searchOrigin)
+        {
+            return new HtmlDocument();
         }
     }
 }
