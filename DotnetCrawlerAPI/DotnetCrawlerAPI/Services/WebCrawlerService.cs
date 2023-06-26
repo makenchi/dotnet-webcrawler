4cd988b950ebec417171cdcf3ee2660645fe8213
makenchi <lucas.santos9322@gmail.com>
2023-06-25 21:21:35 -0300

    feat: implementando crawler

--- DotnetCrawlerAPI/DotnetCrawlerAPI/Services/WebCrawlerService.cs
+++ DotnetCrawlerAPI/DotnetCrawlerAPI/Services/WebCrawlerService.cs
@@ -2,6 +2,7 @@
 using DotnetCrawlerAPI.Entities;
 using DotnetCrawlerAPI.Services.Interfaces;
 using HtmlAgilityPack;
+using System.Net;
 
 namespace DotnetCrawlerAPI.Services
 {
@@ -9,6 +10,8 @@ namespace DotnetCrawlerAPI.Services
     {
         public async Task<List<ProductDTO>> SearchProducts(string searchOrigin, string query)
         {
+            var products = getProducts(searchOrigin, query);
+
             throw new NotImplementedException();
         }
 
@@ -19,12 +22,25 @@ namespace DotnetCrawlerAPI.Services
 
         private List<Product> getProducts(string searchOrigin, string query)
         {
+            HtmlDocument page = crawle(string.Format("{0}{1}",searchOrigin,query));
+
             return new List<Product>();
         }
 
         private HtmlDocument crawle(string searchOrigin)
         {
-            return new HtmlDocument();
+            HtmlDocument doc = new HtmlDocument();
+            using (WebClient client = new WebClient())
+            {
+                string origin = client.DownloadString(searchOrigin);
+
+                var htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(origin);
+
+                doc = htmlDoc;
+            }
+
+            return doc;
         }
     }
 }
